3bbebb531a40085ebe9900c420746688b060073e
neel-1905 <neelnarwadkar@gmail.com>
2026-05-21 12:32:11 +0530

    prisma client setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 import configuration from './config/configuration';
 
 @Module({
   imports: [ConfigModule.forRoot({
     load: [configuration]
-  })],
+  }), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
